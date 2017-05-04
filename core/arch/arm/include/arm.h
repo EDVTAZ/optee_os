8a3f1049ed9b233529eb8aa3ed77eca194c53e51
Etienne Carriere <etienne.carriere@linaro.org>
2017-05-04 14:49:38 +0200

    core: extend MPIDR support
    MPIDR_* macros are dumped from the the ATF project.
    
    get_core_pos_by_mpidr() acts as get_core_pos() but each any MPIDR
    value. This is implemented only for the AArch32, as only this architecture
    will use it in the PSCI library.
    
    Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>

--- core/arch/arm/include/arm.h
+++ core/arch/arm/include/arm.h
@@ -27,10 +27,55 @@
 #ifndef ARM_H
 #define ARM_H
 
+#define MIDR_IMPL_MASK		0xff
+#define MIDR_IMPL_SHIFT		0x18
+#define MIDR_VAR_SHIFT		20
+#define MIDR_VAR_BITS		4
+#define MIDR_VAR_MASK		0xf
+#define MIDR_REV_SHIFT		0
+#define MIDR_REV_BITS		4
+#define MIDR_REV_MASK		0xf
+#define MIDR_PN_MASK		0xfff
+#define MIDR_PN_SHIFT		0x4
 
-#define MPIDR_CPU_MASK		0xff
-#define MPIDR_CLUSTER_SHIFT	8
-#define MPIDR_CLUSTER_MASK	(0xff << MPIDR_CLUSTER_SHIFT)
+#define MPIDR_CPU_MASK		MPIDR_AFFLVL_MASK
+#define MPIDR_CLUSTER_MASK	MPIDR_AFFLVL_MASK << MPIDR_AFFINITY_BITS
+#define MPIDR_AFFINITY_BITS	8
+#define MPIDR_AFFLVL_MASK	0xff
+#define MPIDR_AFFLVL_SHIFT	3
+#define MPIDR_AFF0_SHIFT	0
+#define MPIDR_AFF1_SHIFT	8
+#define MPIDR_AFF2_SHIFT	16
+#ifdef CFG_ARM32_core
+#define MPIDR_AFFINITY_MASK	0x00ffffff
+#else
+#define MPIDR_AFF3_SHIFT	32
+#define MPIDR_AFFINITY_MASK	0xff00ffffff
+#endif
+#define MPIDR_AFFLVL0		0
+#define MPIDR_AFFLVL1		1
+#define MPIDR_AFFLVL2		2
+#define MPIDR_AFFLVL3		3
+#define MPIDR_AFFLVL0_VAL(mpidr) \
+		((mpidr >> MPIDR_AFF0_SHIFT) & MPIDR_AFFLVL_MASK)
+#define MPIDR_AFFLVL1_VAL(mpidr) \
+		((mpidr >> MPIDR_AFF1_SHIFT) & MPIDR_AFFLVL_MASK)
+#define MPIDR_AFFLVL2_VAL(mpidr) \
+		((mpidr >> MPIDR_AFF2_SHIFT) & MPIDR_AFFLVL_MASK)
+
+#ifdef CFG_ARM64_core
+#define MPIDR_AFFLVL3_VAL(mpidr) \
+		((mpidr >> MPIDR_AFF3_SHIFT) & MPIDR_AFFLVL_MASK)
+#endif
+
+/*
+ * The MPIDR_MAX_AFFLVL count starts from 0. Take care to
+ * add one while using this macro to define array sizes.
+ */
+#define MPIDR_MAX_AFFLVL	2
+
+/* OP-TEE legacy macros */
+#define MPIDR_CLUSTER_SHIFT	MPIDR_AFFINITY_BITS
 
 
 /* CLIDR definitions */
