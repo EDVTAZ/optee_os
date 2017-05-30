b176b6d8f3a4a5acd99862359edf0855a595b2b0
Etienne Carriere <etienne.carriere@linaro.org>
2017-05-30 17:47:54 +0200

    libpsci: fix some types, cast and coding style
    Add required '__unused' and '__maybe_unused'.
    Use macros 'FUNC' and 'END_FUNC' in assembly sources.
    Replace use of macro 'stcopr' with macro 'write_xxx' in assembly sources.
    Fix some casts or variable types.
    
    Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>

--- core/arch/arm/libpsci/optee_port/plat_psci_common.c
+++ core/arch/arm/libpsci/optee_port/plat_psci_common.c
@@ -159,7 +159,7 @@ u_register_t plat_psci_stat_get_residency(unsigned int lvl,
  * coordinated target local power state for a power domain will be the minimum
  * of the requested local power states.
  */
-plat_local_state_t plat_get_target_pwr_state(unsigned int lvl,
+plat_local_state_t plat_get_target_pwr_state(unsigned int lvl __maybe_unused,
 					     const plat_local_state_t *states,
 					     unsigned int ncpu)
 {
